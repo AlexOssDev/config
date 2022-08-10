c1b352cec65985da6ab7c5c83900f9f8819c9fb8
alexdev <alexossdev@gmail.com>
2022-08-10 15:28:23 +0300

    Added the nvim config

---
+++ dotfiles/nvim/init.lua
@@ -0,0 +1,41 @@
+-- necessary variables
+api = vim.api
+opt = vim.opt
+
+-- Basic config
+opt.tabstop     = 4
+opt.shiftwidth  = 4
+opt.softtabstop = 4
+opt.expandtab   = true
+
+opt.nu = true
+opt.relativenumber = true
+
+-- Bindings
+function map(sequence, cmd)
+    api.nvim_set_keymap('n',
+    '<Space>' .. sequence,
+    cmd,
+    { noremap = true })
+end
+
+map('o', ':FZF<CR>')
+map('s', '<C-^>')
+map('rr', ':RustRun<CR>')
+map('rf', ':RustFmt<CR>')
+
+-- opt.termguicolors = true
+pcall(vim.cmd, 'colorscheme nord')
+vim.g.lightline = { colorscheme = 'nord' }
+
+return require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+
+    -- Colorscheme and status
+    use 'arcticicestudio/nord-vim'
+    use 'itchyny/lightline.vim'
+
+    -- Search
+    use 'junegunn/fzf'
+end)
+
